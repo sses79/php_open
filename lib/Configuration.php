498e620cdeb15a93e15d6549eaf2d4f8aa143b85
sses79 <sses79@hotmail.com>
2021-02-04 13:33:54 +0000

    Use base_url() get $host

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -81,7 +81,7 @@ class Configuration
      *
      * @var string
      */
-    protected $host = 'http://passkit.test';
+    protected $host = base_url();
 
     /**
      * User agent of the HTTP request, set to "OpenAPI-Generator/{version}/PHP" by default
@@ -436,7 +436,7 @@ class Configuration
     {
         return array(
           array(
-            "url" => "http://passkit.test/",
+            "url" => "http://passgen.test",
             "description" => "No description provided",
           )
         );
